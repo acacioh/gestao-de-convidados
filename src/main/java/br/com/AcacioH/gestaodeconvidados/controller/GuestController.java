b5fd0b2d8ddf7baad64ffbaa470bee52674e99b8
AcacioH <acaciohenriqueafonso@gmail.com>
2021-06-28 20:36:39 -0300

    Criacao do controller

---
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/controller/GuestController.java
@@ -0,0 +1,23 @@
+package br.com.AcacioH.gestaodeconvidados.controller;
+
+import br.com.AcacioH.gestaodeconvidados.dto.GuestDto;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuestController {
+    public GuestDto create(GuestDto guest) {
+        return null;
+    }
+
+    public GuestDto read(Integer id) {
+        return null;
+    }
+
+    public GuestDto update(GuestDto guest) {
+        return null;
+    }
+
+    public void delete(Integer id) {
+
+    }
+}
