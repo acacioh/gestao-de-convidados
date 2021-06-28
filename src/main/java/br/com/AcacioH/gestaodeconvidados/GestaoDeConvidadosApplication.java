c8f83e2f0e0fb02d0a72bdbfab5c65bda625c0bc
AcacioH <acaciohenriqueafonso@gmail.com>
2021-06-28 20:11:47 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/GestaoDeConvidadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.AcacioH.gestaodeconvidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeConvidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeConvidadosApplication.class, args);
+	}
+
+}
