ed1981f68cf89651c011f2b8a10d647ecb0ec42a
AcacioH <acaciohenriqueafonso@gmail.com>
2021-06-28 20:39:54 -0300

    Add service

--- src/main/java/br/com/AcacioH/gestaodeconvidados/controller/GuestController.java
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/controller/GuestController.java
@@ -1,23 +1,28 @@
 package br.com.AcacioH.gestaodeconvidados.controller;
 
 import br.com.AcacioH.gestaodeconvidados.dto.GuestDto;
+import br.com.AcacioH.gestaodeconvidados.service.GuestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GuestController {
+    @Autowired
+    private GuestService guestService;
+
     public GuestDto create(GuestDto guest) {
-        return null;
+        return guestService.create(guest);
     }
 
     public GuestDto read(Integer id) {
-        return null;
+        return guestService.read(id);
     }
 
     public GuestDto update(GuestDto guest) {
-        return null;
+        return guestService.update(guest);
     }
 
     public void delete(Integer id) {
-
+        guestService.delete(id);
     }
 }
---
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/service/GuestService.java
@@ -0,0 +1,21 @@
+package br.com.AcacioH.gestaodeconvidados.service;
+
+import br.com.AcacioH.gestaodeconvidados.dto.GuestDto;
+
+public class GuestService {
+    public GuestDto create(GuestDto guest) {
+        return null;
+    }
+
+    public GuestDto read(Integer id) {
+        return null;
+    }
+
+    public GuestDto update(GuestDto guest) {
+        return null;
+    }
+
+    public void delete(Integer id) {
+
+    }
+}
