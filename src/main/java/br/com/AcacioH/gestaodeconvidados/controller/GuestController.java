6716846654878eaa10f64deb1c1f4092e269cf18
AcacioH <acaciohenriqueafonso@gmail.com>
2021-07-11 09:35:41 -0300

    Adiciona validacao de parametros no endpoint

--- src/main/java/br/com/AcacioH/gestaodeconvidados/controller/GuestController.java
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/controller/GuestController.java
@@ -2,9 +2,15 @@ package br.com.AcacioH.gestaodeconvidados.controller;
 
 import br.com.AcacioH.gestaodeconvidados.model.Guest;
 import br.com.AcacioH.gestaodeconvidados.service.GuestService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("guests")
@@ -14,27 +20,33 @@ public class GuestController {
     private GuestService guestService;
 
     @GetMapping
-    public Page<Guest> findAll(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size) {
+    @ApiOperation("Retorna a lista de convidados paginada")
+    public Page<Guest> findAll(@ApiParam("Número da página iniciado em 0") @RequestParam(defaultValue = "0") @Min(0) Integer page,
+                               @ApiParam("Tamanho da página") @RequestParam(defaultValue = "10") @Min(1) @Max(100) Integer size) {
         return guestService.findAll(page, size);
     }
 
     @PostMapping
-    public Guest create(String name) {
+    @ApiOperation("Cadastra um novo convidado")
+    public Guest create(@ApiParam("Nome do convidado") @NotBlank String name) {
         return guestService.create(name);
     }
 
     @GetMapping("{id}")
-    public Guest findById(@PathVariable Integer id) {
+    @ApiOperation("Retorna as informações de um convidado específico")
+    public Guest findById(@ApiParam("Identificador de um convidado") @PathVariable Integer id) {
         return guestService.findById(id);
     }
 
     @PutMapping
-    public Guest update(Guest guest) {
+    @ApiOperation("Atualiza as informações de um convidado")
+    public Guest update(@RequestBody Guest guest) {
         return guestService.update(guest);
     }
 
     @DeleteMapping
-    public void delete(Integer id) {
+    @ApiOperation("Remove um convidado da lista de convidados")
+    public void delete(@ApiParam("Identificador de um convidado") Integer id) {
         guestService.delete(id);
     }
 }
