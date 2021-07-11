32ecf2d999fc7b89fb8e7bc8d1eb327fe471238d
AcacioH <acaciohenriqueafonso@gmail.com>
2021-07-11 08:40:42 -0300

    Valida se um convidado existe

--- src/main/java/br/com/AcacioH/gestaodeconvidados/service/GuestService.java
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/service/GuestService.java
@@ -1,5 +1,6 @@
 package br.com.AcacioH.gestaodeconvidados.service;
 
+import br.com.AcacioH.gestaodeconvidados.exception.NotFoundException;
 import br.com.AcacioH.gestaodeconvidados.model.Guest;
 import br.com.AcacioH.gestaodeconvidados.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +16,19 @@ public class GuestService {
     public Guest create(String name) {
         Guest g = new Guest();
         g.setName(name);
-
         Guest guestSaved = guestRepository.save(g);
         return guestSaved;
     }
 
     public Guest findById(Integer id) {
-        Guest guest = guestRepository.findById(id).orElse(null);
+        Guest guest = getValidGuest(id);
+        return guest;
+    }
+
+    private Guest getValidGuest(Integer id) {
+        // Verify if a guest exists or throw exception
+        Guest guest = guestRepository.findById(id).orElseThrow(() -> new NotFoundException());
+
         return guest;
     }
 
