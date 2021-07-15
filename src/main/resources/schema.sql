77d9a6bd0b6d5d7820b8b1c756ec8467fc96ea25
AcacioH <acaciohenriqueafonso@gmail.com>
2021-07-15 07:54:48 -0300

    Adiciona tabela de usuario

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,3 +2,9 @@ create table guest (
   id int auto_increment primary key,
   name varchar(250) not null
 );
+
+create table application_user (
+  id int auto_increment primary key,
+  username varchar(250) not null,
+  password varchar(250) not null
+);
