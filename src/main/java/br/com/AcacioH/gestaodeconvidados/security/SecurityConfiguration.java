67ddd36ab3751cade48b69cae19e436a8374ec6c
AcacioH <acaciohenriqueafonso@gmail.com>
2021-07-15 21:37:29 -0300

    Remove cors

--- src/main/java/br/com/AcacioH/gestaodeconvidados/security/SecurityConfiguration.java
+++ src/main/java/br/com/AcacioH/gestaodeconvidados/security/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .antMatchers("/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs/**").permitAll()
